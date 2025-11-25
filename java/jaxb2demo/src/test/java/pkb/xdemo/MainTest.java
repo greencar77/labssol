ca0ea008496f85c502239ec6d0f40dab3feee1ee
greencar77 <x>
2025-11-25 10:03:26 +0200

    jaxb

--- java/jaxb2demo/src/test/java/pkb/xdemo/MainTest.java
+++ java/jaxb2demo/src/test/java/pkb/xdemo/MainTest.java
@@ -2,9 +2,23 @@ package pkb.xdemo;
 
 import org.junit.jupiter.api.Test;
 
+import com.something.CompTypeWithSeq;
+import com.something.UserRequest;
+import com.something.UserResponse;
+import com.something.WarningsType;
+
 public class MainTest {
 
 	@Test
 	public void test() {
+		UserRequest userRequest = new UserRequest();
+		userRequest.setName("John");
+
+		UserResponse userResponse = new UserResponse();
+		CompTypeWithSeq comp = new CompTypeWithSeq();
+		WarningsType warnings = new WarningsType();
+		warnings.setMessage("Error 55");
+		comp.getWarningsAndErrors().add(warnings);
+		userResponse.setCompSeq(comp);
 	}
 }
